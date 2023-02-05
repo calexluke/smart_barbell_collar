9421fc8d152090a53eadf4a58a6ed72aa2ec3748
calexluke <c.alexluke@gmail.com>
2023-02-05 13:13:17 -0600

    restrict app to run in portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:smart_barbell_collar/home_view.dart';
 
 void main() {
@@ -11,6 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Smart Barbell Collar',
       theme: ThemeData.dark().copyWith(
